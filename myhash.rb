fb2ef6f263fe936c1ccbef7f4355a1ba05939034
Max Obushenko <max.obushenko@gmail.com>
2016-11-21 14:20:40 +0200

    added Set data for keys

--- myhash.rb
+++ myhash.rb
@@ -1,4 +1,25 @@
+# Hash class
+require 'pry'
+require 'set'
+
 class MyHash
   def initialize
+    @keys = Set.new
+    @values = Array.new
+   end
+
+  def [](symbol)
+    if @keys.include?(symbol)
+      @values.each do |k, v|
+        if symbol == k
+          puts v
+        end
+      end
+    end
+  end
+
+  def []=(key, val)
+    @keys.add(key)
+    @values << val
   end
 end
